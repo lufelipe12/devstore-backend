051e729db81a1944c22733be195028fa227756ff
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-30 17:47:32 -0300

    feat: added new way to create user and fuctions to find an user by email and id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,18 +6,23 @@ import {
   Patch,
   Param,
   Delete,
+  HttpException,
+  HttpStatus,
+  Inject,
+  LoggerService,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.usersService.create(createUserDto);
   }
 
   @Get()
