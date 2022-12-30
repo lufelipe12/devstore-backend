aea61da4781576b2577866e2247c73a9d1e6f65c
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-30 20:46:36 -0300

    feat: added configurations, routes and services for user to login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,38 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+  Res,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
+import { User } from 'src/database/entities/user.entity';
+
 import { AuthService } from './auth.service';
-import { LoginDto } from './dto/login.dto';
+import { CookieHttpConfig } from './configs';
+import { CurrentUser } from './decorators';
+import { LocalAuthGuard } from './guards';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  login(@Body() loginDto: LoginDto) {
-    return this.authService.login(loginDto);
+  @Post('/login')
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  async login(
+    @CurrentUser() currentUser: User,
+    @Res() res: Response,
+  ): Promise<void> {
+    const { token, user } = await this.authService.login(currentUser);
+
+    res
+      .status(HttpStatus.OK)
+      .cookie('Authorization', token, CookieHttpConfig.Options())
+      .json(user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,100 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  LoggerService,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ConfigService } from '@nestjs/config';
+import { Repository } from 'typeorm';
+
+import { User } from '../database/entities/user.entity';
 import { LoginDto } from './dto/login.dto';
+import { AuthTokenPayload } from './interfaces';
+import { JWTService } from '../jwt/jwt.service';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  login(loginDto: LoginDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly logger: LoggerService,
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+
+    private readonly configService: ConfigService,
+    private readonly jwtService: JWTService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async login(payload: AuthTokenPayload): Promise<any> {
+    const currentUser: User = await this.usersService.findOne(payload.id);
+
+    const newPayload = {
+      id: currentUser.id,
+      name: currentUser.name,
+      email: currentUser.email,
+    };
+
+    const token: string = await this.getToken(newPayload);
+
+    return { token, user: currentUser };
+  }
+
+  async getToken(
+    tokenPayload: AuthTokenPayload,
+    expires?: string,
+  ): Promise<string> {
+    try {
+      const token: string = await this.jwtService.signAsync(tokenPayload, {
+        expiresIn:
+          expires || this.configService.get<number>('JWT_EXPIRATION') * 1000,
+        secret: this.configService.get<string>('JWT_SECRET'),
+      });
+      return `Bearer ${token}`;
+    } catch (error) {
+      this.logger.error(error);
+
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async validateUser(email: string, password: string): Promise<User> {
+    try {
+      const user: User = await this.usersRepository.findOne({
+        where: [{ email: email }],
+        select: ['id', 'name', 'email', 'password'],
+      });
+
+      if (!user) {
+        throw new UnauthorizedException(`User not found`);
+      }
+
+      const passwordIsValid = await bcrypt.compare(
+        password,
+        user?.password || '',
+      );
+
+      if (!passwordIsValid) {
+        throw new UnauthorizedException('Credentials are not valid.');
+      }
+
+      const { password: _, ...result } = user;
+
+      return result as User;
+    } catch (error) {
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
