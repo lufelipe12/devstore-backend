6be9bcb459ad3cb98fd954652722baa04e056e14
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-30 12:07:41 -0300

    feat: added providers in module for products

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { ProviderOneClient, ProviderTwoClient } from 'src/utils/resources';
 
 @Module({
+  imports: [],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, ProviderOneClient, ProviderTwoClient],
 })
 export class ProductsModule {}
