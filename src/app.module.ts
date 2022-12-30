2cf3115838b605ab5f947de8c4a7d26100f32007
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-30 17:01:54 -0300

    feat: added carts module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { DatabaseModule } from './database/database.module';
 import { LoggerInterceptor } from './logger/logger.interceptor';
 import { winstonAsyncConfigOptions } from './logger/winston.config';
 import { ProductsModule } from './products/products.module';
+import { CartsModule } from './carts/carts.module';
 
 @Module({
   imports: [
     WinstonModule.forRootAsync(winstonAsyncConfigOptions),
     DatabaseModule,
     ProductsModule,
+    CartsModule,
   ],
   providers: [
     {
