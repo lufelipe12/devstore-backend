0b2e5d9108c2d530f449bb29a8d1b80291568982
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-30 17:46:56 -0300

    update: update create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,27 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 50)
+  name: string;
+
+  @IsEmail()
+  @Length(5, 50)
+  @IsNotEmpty()
+  email: string;
+
+  @IsOptional()
+  isAdmin: boolean;
+
+  @IsString()
+  @Length(5, 200)
+  @IsNotEmpty()
+  password: string;
+}
