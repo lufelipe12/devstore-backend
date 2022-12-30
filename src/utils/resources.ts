cc1d428c4f129c76ec2ef49ed6f0e2913ff6931b
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-30 10:37:51 -0300

    feat: added utils for providers

---
+++ src/utils/resources.ts
@@ -0,0 +1,5 @@
+export const PROVIDER_ONE =
+  'http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/brazilian_provider';
+
+export const PROVIDER_TWO =
+  'http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/european_provider';
