f9f4aa4685a234b18a5145486a6788cb865cb5e3
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-30 10:40:47 -0300

    fix: added utils for providers

--- src/utils/resources.ts
+++ src/utils/resources.ts
@@ -1,5 +1,11 @@
-export const PROVIDER_ONE =
-  'http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/brazilian_provider';
+import axios from 'axios';
 
-export const PROVIDER_TWO =
-  'http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/european_provider';
+export const providerOne = axios.create({
+  baseURL:
+    'http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/brazilian_provider',
+});
+
+export const providerTwo = axios.create({
+  baseURL:
+    ' http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/european_provider',
+});
