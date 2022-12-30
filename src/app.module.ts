5766fdc5f57248b0ce6475e0b7273acd9bc33a18
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-30 10:24:50 -0300

    feat: added database and winston configuration in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
+
+import { DatabaseModule } from './database/database.module';
+import { LoggerInterceptor } from './logger/logger.interceptor';
+import { winstonAsyncConfigOptions } from './logger/winston.config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    WinstonModule.forRootAsync(winstonAsyncConfigOptions),
+    DatabaseModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
