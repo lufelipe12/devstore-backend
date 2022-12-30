1d0dfd7677ef303e44249a40cfb9c9844a9566c3
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-30 14:18:12 -0300

    feat: added varparser on product module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { ProviderOneClient, ProviderTwoClient } from 'src/utils/resources';
+import { VarParsers } from 'src/utils/parsers';
 
 @Module({
   imports: [],
   controllers: [ProductsController],
-  providers: [ProductsService, ProviderOneClient, ProviderTwoClient],
+  providers: [
+    ProductsService,
+    ProviderOneClient,
+    ProviderTwoClient,
+    VarParsers,
+  ],
 })
 export class ProductsModule {}
