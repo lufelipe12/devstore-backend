ef4319babfe67449ddfb1a6c3abd13d6b4ff7d0f
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-30 19:39:33 -0300

    feat: added new auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { winstonAsyncConfigOptions } from './logger/winston.config';
 import { ProductsModule } from './products/products.module';
 import { CartsModule } from './carts/carts.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UsersModule } from './users/users.module';
     ProductsModule,
     CartsModule,
     UsersModule,
+    AuthModule,
   ],
   providers: [
     {
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+@ApiTags('auth')
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
