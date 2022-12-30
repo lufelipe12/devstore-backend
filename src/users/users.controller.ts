3ac1f1dfabb28f159a2b0af2b201c7d70c8049fd
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-30 19:34:54 -0300

    feat: added more docs in users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,7 +12,13 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiCookieAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserCreatedResponseDoc, UserRequestDoc } from 'src/docs';
 
 @ApiTags('users')
@@ -38,21 +44,44 @@ export class UsersController {
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
+  @ApiCookieAuth()
+  @ApiOperation({
+    summary: 'Find all users.',
+    description: 'Find all users.',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    isArray: true,
+    type: UserCreatedResponseDoc,
+  })
   async findAll() {
     return await this.usersService.findAll();
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  @ApiCookieAuth()
+  @ApiOperation({
+    summary: 'Find an user by id.',
+    description: 'Find an user by id.',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: UserCreatedResponseDoc,
+  })
   async findOne(@Param('id') id: string) {
     return await this.usersService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiCookieAuth()
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiCookieAuth()
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
