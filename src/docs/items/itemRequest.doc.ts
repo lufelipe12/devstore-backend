3d297195bfbe42950b2a930c799a93068b92746e
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-31 11:41:45 -0300

    feat: added docs in items controller and entities

---
+++ src/docs/items/itemRequest.doc.ts
@@ -0,0 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ProductProvider } from 'src/utils';
+
+export class ItemRequestDoc {
+  @ApiProperty({
+    type: 'string',
+    example: 'Sword',
+    description: 'item name',
+  })
+  name: string;
+
+  @ApiProperty({
+    type: 'number',
+    example: 1000,
+    description: 'item price',
+  })
+  price: string;
+
+  @ApiProperty({
+    type: 'enum',
+    example: ProductProvider.Brazil,
+    description: 'items provider',
+  })
+  provider: ProductProvider;
+
+  @ApiProperty({
+    type: 'boolean',
+    example: false,
+    description: 'if item is on sale or not',
+  })
+  hasDiscount: boolean;
+
+  @ApiProperty({
+    type: 'number',
+    example: 1,
+    description: 'carts id for item',
+  })
+  cartId: number;
+}
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -6,15 +6,34 @@ import {
   Patch,
   Param,
   Delete,
+  HttpStatus,
+  HttpCode,
 } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
 import { ItemsService } from './items.service';
 import { CreateItemDto } from './dto/create-item.dto';
+import { ItemCreatedResponseDoc, ItemRequestDoc } from '../docs';
+import { ItemDeletedResponseDoc } from '../docs';
 
+@ApiTags('items')
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({
+    summary: 'Create new item.',
+    description: 'Create new item, based on providers.',
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    type: ItemCreatedResponseDoc,
+  })
+  @ApiBody({
+    type: ItemRequestDoc,
+  })
   async create(@Body() createItemDto: CreateItemDto) {
     return await this.itemsService.create(createItemDto);
   }
@@ -30,6 +49,15 @@ export class ItemsController {
   // }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.ACCEPTED)
+  @ApiOperation({
+    summary: 'Delete an item.',
+    description: 'Delete an item, based on id.',
+  })
+  @ApiResponse({
+    status: HttpStatus.ACCEPTED,
+    type: ItemDeletedResponseDoc,
+  })
   async remove(@Param('id') id: string) {
     return await this.itemsService.removeFromCart(+id);
   }
