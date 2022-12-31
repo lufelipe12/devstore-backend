b28b87591a827255456f256d9f3d5f333af11a7c
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-12-31 11:21:50 -0300

    feat: added some dependencies in module and async await in calls

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -30,7 +30,7 @@ export class ItemsController {
   // }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.itemsService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return await this.itemsService.removeFromCart(+id);
   }
 }
--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cart } from '../database/entities/cart.entity';
+import { Item } from '../database/entities/item.entity';
+import { CartsModule } from '../carts/carts.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Item]), CartsModule],
   controllers: [ItemsController],
   providers: [ItemsService],
+  exports: [ItemsService],
 })
 export class ItemsModule {}
