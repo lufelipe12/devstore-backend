7da5b77cfb707b0b3a68319c6fdc474615295f2c
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-01-02 10:39:47 -0300

    feat: added configs in purchase module

--- src/purchases/purchases.module.ts
+++ src/purchases/purchases.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { PurchasesService } from './purchases.service';
 import { PurchasesController } from './purchases.controller';
+import { Purchase } from '../database/entities/purchase.entity';
+import { UsersModule } from '../users/users.module';
+import { CartsModule } from '../carts/carts.module';
+import { ItemsModule } from '../items/items.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([Purchase]),
+    UsersModule,
+    CartsModule,
+    ItemsModule,
+  ],
   controllers: [PurchasesController],
   providers: [PurchasesService],
   exports: [PurchasesService],
